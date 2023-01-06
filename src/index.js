4339f724e6bc48bbf71c75aea72f68cce2fd0d3a
ba2dzc <batu.duzcu@gmail.com>
2023-01-06 17:31:44 +0300

    Redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import configureStore from './redux/reducers/configureStore';
+import 'alertifyjs/build/css/alertify.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store=configureStore();
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>
 );
 
